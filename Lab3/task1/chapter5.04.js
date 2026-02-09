ad4800df9daeeed3735dfb21cb865c755339426e
Alexander C. <vempose@gmail.som>
2026-02-09 23:34:05 +0500

    lab3: task1: follow pattern in filenames

---
+++ Lab3/task1/chapter5.04.js
@@ -0,0 +1,66 @@
+// Is array copied?
+let fruits = ["Apples", "Pear", "Orange"];
+
+let shoppingCart = fruits;
+
+shoppingCart.push("Banana");
+
+alert(fruits.length); // 4
+
+// Array operations
+let styles = ["Jazz", "Blues"];
+styles.push("Rock-n-Roll");
+styles[Math.floor((styles.length - 1) / 2)] = "Classics";
+alert(styles.shift());
+styles.unshift("Rap", "Reggae");
+
+// Calling in an array context
+let arr = ["a", "b"];
+
+arr.push(function () {
+  alert(this);
+});
+
+arr[2](); // a,b,function(){...}
+
+// Sum input numbers
+function sumInput() {
+  let numbers = [];
+
+  while (true) {
+    let value = prompt("A number please?", 0);
+
+    if (value === "" || value === null || !isFinite(value)) break;
+
+    numbers.push(+value);
+  }
+
+  let sum = 0;
+  for (let number of numbers) {
+    sum += number;
+  }
+  return sum;
+}
+
+alert(sumInput());
+
+// A maximal subarray
+function getMaxSubSum(arr) {
+  let maxSum = 0;
+  let partialSum = 0;
+
+  for (let item of arr) {
+    partialSum += item;
+    maxSum = Math.max(maxSum, partialSum);
+    if (partialSum < 0) partialSum = 0;
+  }
+
+  return maxSum;
+}
+
+alert(getMaxSubSum([-1, 2, 3, -9])); // 5
+alert(getMaxSubSum([-1, 2, 3, -9, 11])); // 11
+alert(getMaxSubSum([-2, -1, 1, 2])); // 3
+alert(getMaxSubSum([100, -9, 2, -3, 5])); // 100
+alert(getMaxSubSum([1, 2, 3])); // 6
+alert(getMaxSubSum([-1, -2, -3])); // 0
--- Lab3/task1/chapter5.9.js
+++
@@ -1,9 +0,0 @@
-// Sum the properties
-function sumSalaries(salaries) {
-  return Object.values(salaries).reduce((a, b) => a + b, 0);
-}
-
-// Count properties
-function count(obj) {
-  return Object.keys(obj).length;
-}
